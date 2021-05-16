36d52f5f9f5d65f1b99db83d41023d89db2db78c
VDemyanov <vlad.demyanov.official@mail.ru>
2021-05-16 15:42:54 +0300

    Bootstrap

--- general/js/lab10/lab10.js
+++ general/js/lab10/lab10.js
@@ -4,7 +4,7 @@ function lab10() {
     setInterval(() => {
         let now = new Date();
         let newYear = new Date(now.getFullYear() + 1, 0, 1, 0, 0, 0);
-        
+
         let days = Math.floor((newYear - now) / 1000 / 60 / 60 / 24);
         let hours =  Math.floor((newYear - now) / 1000 / 60 / 60 % 24);
         let minutes =  Math.floor((newYear - now) / 1000 / 60 % 60);
